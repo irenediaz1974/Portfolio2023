fc9fe44342c7fcbb5e7e0ae94ea56a19d01e857e
Irene Diaz <irenediazcuba@gmail.com>
2023-06-17 17:56:44 -0400

    1706

--- PizzaStore/PizzaStore/Program.cs
+++ PizzaStore/PizzaStore/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.DB;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
     
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+
 builder.Services.AddSwaggerGen(c =>
 {
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
@@ -17,5 +23,13 @@ app.UseSwaggerUI(c =>
 });
     
 app.MapGet("/", () => "Hello World!");
-    
+
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync()); 
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 app.Run();
